b7e63e0ec9ac45c86e45b718da21d51338c39651
firstfloris <contact@firstart.co>
2022-10-15 15:03:24 +0200

    Run

--- multi_main.py
+++ multi_main.py
@@ -149,8 +149,8 @@ if __name__ == '__main__':
     ######################################## PARAMETERS
     
     # set parameters
-    max_gen = 5
-    pop = 1
+    max_gen = 10000
+    pop = 50
     inc_crossover_operator = True
     enemies = [[1,2,3,4],[5,6,7,8]]
     checkpoint_name = None
